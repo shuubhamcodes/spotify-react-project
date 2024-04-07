985e83bf9279f4c54544c2833cff00f05ea77b9c
shubham <shuubhamsharma98@gmail.com>
2024-04-06 22:43:53 -0500

    add all config and src files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { useEffect } from 'react';
 import './App.css';
 import Login from "./Login"
-
+import { getTokenFromResponse } from './spotify';
 function App() {
+ useEffect(()=>{
+  const token = getTokenFromResponse();
+  console.log("MY TOKEN IS: ",token);
+ },[]);
+
   return (
     <div className="App">
       <h1>
